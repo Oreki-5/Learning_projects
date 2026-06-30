df6e3e309d4bcb92feeba83dec9036c9f5fc0a14
Oreki-5 <adityacoc1602@gmail.com>
2026-06-30 16:21:06 +0530

    [Update]{python_FastAPI_test} Tested simple CRUD operations with in-memory list of objects

--- python_FastAPI_test/main.py
+++ python_FastAPI_test/main.py
@@ -6,6 +6,8 @@ from pydantic import BaseModel
 
 app=FastAPI()
 
+list_of_posts =[]
+
 # Data Model 
 class Post(BaseModel):
     title:str
@@ -16,10 +18,12 @@ class Post(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"Hello": "test"}
+    return list_of_posts
 
 
 @app.post("/posts")
 def create_posts(new_post : Post):
     print(new_post.model_dump())
+    list_of_posts.append(new_post)
+    print(list_of_posts)
     return {"data":"new post"}
